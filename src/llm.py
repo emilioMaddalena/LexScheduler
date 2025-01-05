bd70090c0796496cca7a6dddb7609a82b6c6a1ae
emilioMaddalena <emilio.tanowe.maddalena@gmail.com>
2025-01-05 17:20:53 -0400

    Implement _is_model_available
    - Implemented a method to verify if the requested model if among the ones available in the ollama server

--- src/llm.py
+++ src/llm.py
@@ -1,5 +1,6 @@
 import ollama
 import requests
+from ollama import chat
 
 """
 from ollama import chat
@@ -28,13 +29,18 @@ class Llm:
     
     This class assumes ollama is running in the background, 
     which is a local LLM server. The class then provides an 
+    interface for interacting with the service.
+    """
+
+    def __init__(self, model_name: str):  
+        """Check if ollama is running and if the desired model is available."""  
         self.model_name = model_name
-        self.context = ""
+        # self.context = ""
         # Optionally pre-load model or store a handle
-        self.client = ollama.Client(model=self.model_name)
+        # self.client = ollama.Client(model=self.model_name)
 
     def _is_ollama_running(self):
-        """Check if ollama is running."""
+        """Check if the ollama server is running."""
         successful_response = 200
         try:
             response = requests.get(f"http://{OLLAMA_ADDRESS}:{OLLAMA_PORT}/")
@@ -42,15 +48,21 @@ class Llm:
         except requests.exceptions.ConnectionError:
             return False
 
+    def _is_model_available(self):
+        """Check if the model is available in the server."""
+        models = ollama.list().models
+        model_names = [model.model for model in models]
+        return self.model_name in model_names
+    
     def keep_alive(self):
         pass
 
-    def submit_query(self, prompt: str) -> str:  # noqa: D102
-        # Combine stored context with new prompt
-        combined_prompt = f"{self.context}\n{prompt}"
-        response = self.client.generate(combined_prompt)
-        return response.get("text", "")
+    # def submit_query(self, prompt: str) -> str:  # noqa: D102
+    #     # Combine stored context with new prompt
+    #     combined_prompt = f"{self.context}\n{prompt}"
+    #     response = self.client.generate(combined_prompt)
+    #     return response.get("text", "")
 
-    def append_context(self, additional_context: str):  # noqa: D102
-        # Adds new context to be used in subsequent queries
-        self.context += "\n" + additional_context
+    # def append_context(self, additional_context: str):  # noqa: D102
+    #     # Adds new context to be used in subsequent queries
+    #     self.context += "\n" + additional_context
